50f3ee413f7ef5fb6fecc8cb1e3847c18ea5d1c3
UFahath <u.zfahath49@gmail.com>
2025-03-30 16:58:08 +0530

    'mod'

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import './App.css'
 import About from './Pages/About'
 import { Home } from './Pages/Home'
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import {HashRouter as Router,Routes,Route} from 'react-router-dom'
 import {useEffect} from 'react'
 import { useLocation } from 'react-router-dom'
 import ContactUs from './Pages/ContactUs'
