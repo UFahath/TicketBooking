d3aebaa951d2b0222b5fc7ca5b1197093cd88b69
UFahath <u.zfahath49@gmail.com>
2025-04-27 21:05:33 +0530

    'mod'

--- src/components/FlightPaymentOptions/BookingSuccess.jsx
+++ src/components/FlightPaymentOptions/BookingSuccess.jsx
@@ -1,16 +1,37 @@
 import Footer from "../Footer"
 import { Navbar } from "../Navbar"
 import tickmark from '../../assets/images/tickmark.png'
-
+import {useMemo ,useEffect} from "react"
+import {v4 as uuidv4} from "uuid"
 const BookingSuccess = () => {
+
+  
+  let uniqNumber=useMemo(()=>{
+    let savedRef=localStorage.getItem("Reference Number:");
+    if(!savedRef)
+    {
+      let uniqId=uuidv4().toUpperCase().substring(0,uuidv4().indexOf("-"))
+      localStorage.setItem("Reference Number:",uniqId)
+      return uniqId;
+    }
+    else{
+       return savedRef
+    }
+  },[])
+
+  useEffect(()=>{
+       return ()=>{localStorage.removeItem("Reference Number:")}
+  },[])
+ 
   return (
     <>
     <Navbar/>
-    <div className="container">
+    <div className="container" style={{fontFamily:"Nunito Sans"}}>
       <header className="my-4 text-center d-flex justify-content-center align-items-center">
         <img src={tickmark} alt="tickmark" style={{width:"30px",height:"30px"}}/>
         <h2 className="text-danger ms-2">Booking Confirmed</h2>
       </header>
+      <p className="fs-5 fw-bold">Reference Number : {uniqNumber}</p>
     </div>
     <Footer/>
     </>
